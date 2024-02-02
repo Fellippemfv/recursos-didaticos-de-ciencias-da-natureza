a13a1f51979a16451849b4549fb4467760535375
Fellippe Matheus <fellippe.matheus2014@gmail.com>
2024-02-02 19:56:51 -0400

    update files from mobile header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,6 @@ import { BiHome, BiHelpCircle, BiSearch } from 'react-icons/bi';
 import ThemeSwitch from './ThemeSwitch';
 
 const Navbar = () => {
-  const router = useRouter();  // Utilizando useAppRouter
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
@@ -15,7 +14,7 @@ const Navbar = () => {
   return (
     <ul className="hidden sm:flex flex items-center space-x-4 leading-5 sm:space-x-6">
       <li className="nav-item">
-        <Link href="/" className="relative flex items-center py-2 pr-1 hover:text-purple-600 hover:after:absolute hover:after:bottom-0 hover:after:left-0 hover:after:w-full hover:after:h-0.5 hover:after:bg-purple-600 hover:after:content-[''] hover:after:transition-all hover:after:duration-300 focus:outline-none transition duration-300">
+        <Link href="/" className="relative flex items-center py-2 pr-1">
           <BiHome className="mr-2" />
           <span className="text-base leading-6 font-medium text-secondary-500">
             Início
@@ -23,7 +22,7 @@ const Navbar = () => {
         </Link>
       </li>
       <li className="nav-item">
-        <Link href="/pesquisar" className="relative flex items-center py-2 pr-2 hover:text-purple-600 hover:after:absolute hover:after:bottom-0 hover:after:left-0 hover:after:w-full hover:after:h-0.5 hover:after:bg-purple-600 hover:after:content-[''] hover:after:transition-all hover:after:duration-300 focus:outline-none transition duration-300">
+        <Link href="/pesquisar" className="relative flex items-center py-2 pr-2">
           <BiSearch className="mr-2" />
           <span className="text-base leading-6 font-medium text-secondary-500">
             Pesquisar
@@ -31,7 +30,7 @@ const Navbar = () => {
         </Link>
       </li>
       <li className="nav-item">
-        <Link href="/sobre" className="relative flex items-center py-2 pr-2 hover:text-purple-600 hover:after:absolute hover:after:bottom-0 hover:after:left-0 hover:after:w-full hover:after:h-0.5 hover:after:bg-purple-600 hover:after:content-[''] hover:after:transition-all hover:after:duration-300 focus:outline-none transition duration-300">
+        <Link href="/sobre" className="relative flex items-center py-2 pr-2">
           <BiHelpCircle className="mr-2" />
           <span className="text-base leading-6 font-medium text-secondary-500">
             Sobre
