efbe13e02ac88e699d73c0a674e1193634cef5b7
Fellippe Matheus <fellippe.matheus2014@gmail.com>
2024-02-03 11:12:59 -0400

    update files: update header deatils

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/navigation';  // Usando useAppRouter
 import { useEffect, useState } from 'react';
 import { BiHome, BiHelpCircle, BiSearch } from 'react-icons/bi';
 import ThemeSwitch from './ThemeSwitch';
+import { FcAbout, FcHome, FcInfo, FcSearch } from 'react-icons/fc';
 
 const Navbar = () => {
   const [mounted, setMounted] = useState(false);
@@ -14,27 +15,36 @@ const Navbar = () => {
   return (
     <ul className="hidden sm:flex flex items-center space-x-4 leading-5 sm:space-x-6">
       <li className="nav-item">
-        <Link href="/" className="relative flex items-center py-2 pr-1">
-          <BiHome className="mr-2" />
+        <Link href="/" passHref>
+
+        <div className="relative flex items-center py-2 pr-2 transition-all duration-300 transform-gpu hover:shadow-md">
+        <FcHome className="mr-2" />
           <span className="text-base leading-6 font-medium text-secondary-500">
             Início
           </span>
+        </div>
+         
         </Link>
       </li>
       <li className="nav-item">
-        <Link href="/pesquisar" className="relative flex items-center py-2 pr-2">
-          <BiSearch className="mr-2" />
+        <Link href="/pesquisar" passHref>
+        <div className="relative flex items-center py-2 pr-2 transition-all duration-300 transform-gpu hover:shadow-md">
+        <FcSearch className="mr-2" />
           <span className="text-base leading-6 font-medium text-secondary-500">
             Pesquisar
           </span>
+        </div>
+        
         </Link>
       </li>
       <li className="nav-item">
-        <Link href="/sobre" className="relative flex items-center py-2 pr-2">
-          <BiHelpCircle className="mr-2" />
-          <span className="text-base leading-6 font-medium text-secondary-500">
-            Sobre
-          </span>
+        <Link href="/sobre" passHref>
+          <div className="relative flex items-center py-2 pr-2 transition-all duration-300 transform-gpu hover:shadow-md">
+            <FcInfo className="mr-2" />
+            <span className="text-base leading-6 font-medium text-secondary-500">
+              Sobre
+            </span>
+          </div>
         </Link>
       </li>
       <li className="nav-item">
