3ca76985018812d4996a7f56ae7513bca64fc341
Fellippe Matheus <fellippe.matheus2014@gmail.com>
2024-02-04 22:51:49 -0400

    update files in main app

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,16 +4,20 @@ import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import Navbar from './Navbar'
 import { FcBiohazard, FcGlobe, FcSearch } from 'react-icons/fc'
+import { BiSolidFlask } from 'react-icons/bi'
 
 const Header = () => {
     return (
       <header className="flex items-center justify-between py-10">
-      <div className="flex items-center">
-        <FcBiohazard  className="text-blue-500 h-14 w-14 mr-2" />
-        <div>
-          <p className="font-bold flex items-center space-x-4 leading-5 sm:space-x-6">Ciências</p>
-        </div>
-      </div>
+<div className="flex items-center">
+  <BiSolidFlask className="text-green-600 h-14 w-14" />
+  <div className="text-center">
+    <p className="font-serif font-extrabold text-3xl text-green-800">Ciências</p>
+    <p className="font-sans font-normal text-lg text-gray-600">para todos</p>
+  </div>
+</div>
+
+
       <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
         {/* Restante do seu código Navbar e MobileNav */}
       </div>
