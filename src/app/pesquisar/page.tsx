63c38591ec2ff08d335657a64e541f5b9c25640f
Fellippe Matheus <fellippe.matheus2014@gmail.com>
2024-02-09 14:30:34 -0400

    update search page: searh button

--- src/app/pesquisar/page.tsx
+++ src/app/pesquisar/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
 import {
@@ -12,10 +13,32 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 import { Checkbox } from "@/components/ui/checkbox"
+import { useState } from "react";
+import { HiOutlineSearch } from "react-icons/hi";
+
 
 
 export default function Search() {
+
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleSelectChange = (option: any) => {
+    setSelectedOption(option);
+  };
+
+  
+
   return (
 <main className="flex min-h-screen flex-col items-center justify-between p-4">
   <div className="w-full max-w-screen-lg mx-auto mb-8">
@@ -28,28 +51,42 @@ export default function Search() {
     <label htmlFor="search" className="mb-4 text-gray-600 mb-2 block text-xl font-semibold">Escolha uma opção</label>
     <div className=" flex items-center flex-col md:flex-row">
     <div className="min-w-60 mb-4 md:mb-0 md:w-auto w-full ">
-  <select className="w-full rounded border border-gray-300 py-2 px-2 md:px-4 focus:outline-none focus:border-blue-500">
-    <option value="experimentos">Experimentos</option>
-    <option value="demonstracoes">Demonstrações</option>
-  </select>
+
+     <Select onValueChange={handleSelectChange}>
+        <SelectTrigger className="w-[180px] w-full">
+          <SelectValue placeholder="Selecione uma opção" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Opções</SelectLabel>
+            <SelectItem className="cursor-pointer" value="apple">Experimentos</SelectItem>
+            <SelectItem className="cursor-pointer" value="banana">Demonstrações</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
 </div>
 
 
       <div className="w-full flex flex-col md:flex-row mx-auto">
-        <button className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-2 md:mb-0 md:ml-2">Buscar</button>
+      <button 
+        className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-2 md:mb-0 md:ml-2 ${selectedOption ? '' : 'opacity-50 cursor-not-allowed'}`} 
+        disabled={!selectedOption}
+      >
+        <HiOutlineSearch className="inline mr-1" />
+        Buscar
+      </button>
         <AlertDialog>
   <AlertDialogTrigger className="ml-0 md:ml-2 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Resetar Filtros</AlertDialogTrigger>
   <AlertDialogContent>
     <AlertDialogHeader>
-      <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+      <AlertDialogTitle>Você tem certeza?</AlertDialogTitle>
       <AlertDialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
+        Confirmando você vai tirar todos os filtros aplicados
       </AlertDialogDescription>
     </AlertDialogHeader>
     <AlertDialogFooter>
-      <AlertDialogCancel>Cancel</AlertDialogCancel>
-      <AlertDialogAction>Continue</AlertDialogAction>
+      <AlertDialogCancel>Cancelar</AlertDialogCancel>
+      <AlertDialogAction className="">Confirmar</AlertDialogAction>
     </AlertDialogFooter>
   </AlertDialogContent>
 </AlertDialog>
