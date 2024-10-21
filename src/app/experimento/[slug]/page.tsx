e3bb15a4d2b66bfa3286606a5ab82f33b5dbac3a
Fellippe <fellippe.matheus2014@gmail.com>
2024-10-20 20:23:58 -0400

    update files - 20/10/24-3

--- src/app/experimento/[slug]/page.tsx
+++ src/app/experimento/[slug]/page.tsx
@@ -1,9 +1,23 @@
 "use client";
-import { FaCheckCircle } from "react-icons/fa";
+import { FaArrowRight, FaBook, FaChartBar, FaCheckCircle, FaClipboardCheck, FaClipboardList, FaFileAlt, FaFileDownload, FaFlask, FaImage, FaTimes } from "react-icons/fa";
 import experimentData from "../../api/data/experimentos.json";
 import { useState } from "react";
 
 export default function Experiment({ params }: { params: { slug: string } }) {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentImage, setCurrentImage] = useState('');
+
+  const openModal = (imagePath) => {
+    setCurrentImage(imagePath);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setCurrentImage('');
+  };
+
   const [experimentList] = useState(experimentData);
   const pageSlug = params.slug;
 
@@ -27,7 +41,6 @@ export default function Experiment({ params }: { params: { slug: string } }) {
 
   return (
     <div className="container max-w-screen-xl bg-white rounded-lg p-8  border border-gray-300 rounded p-4">
-      <div className="">
         <div
           className="h-40 bg-cover bg-center relative"
           style={{ backgroundImage: `url(${experimentInfo.imagePreview})` }}
@@ -41,206 +54,193 @@ export default function Experiment({ params }: { params: { slug: string } }) {
         </div>
 
         <div className="container border border-gray-300 rounded p-4 bg-white">
-          <div className="flex justify-between items-initial">
-            <div>
-              <p className="text-gray-600">
-                Publicado em: {experimentInfo.postDate}
-              </p>
-              <p className="text-gray-600">
-                Enviado por: {experimentInfo.profileName}
-              </p>
-            </div>
-            <p className="text-blue-500 font-semibold">
-              ID: {experimentInfo.id}
-            </p>
-          </div>
-
-          <h2 className="text-sm font-semibold mt-8">Sobre o experimento</h2>
-        <p className="mt-4 text-lg text-gray-700">
-          {experimentInfo.description}
-        </p>
-        </div>
-
-        
-      </div>
-
-    
-      <div className="mt-8 border border-gray-300 rounded p-4">
-        <div className="bg-gray-50 px-4 py-2 text-center font-medium text-gray-500 uppercase tracking-wider">
-        <h2 className="text-sm font-semibold">Ficha técnica</h2>
-
-        </div>
-
-        <div className="mt-4">
-          <table className="min-w-full text-xs divide-y divide-gray-200">
-           
-            <tbody className="bg-white divide-y divide-gray-200">
-              <tr>
-                <td className="px-4 py-2 font-semibold">
-                  Título do Experimento:
-                </td>
-                <td className="px-4 py-2">{experimentInfo.title}</td>
-              </tr>
-              <tr>
-                <td className="px-4 py-2 font-semibold">
-                  Tipo de Experimento:
-                </td>
-                <td className="px-4 py-2">
-                  <strong>{experimentInfo.experimentType.title}</strong>:{" "}
-                  {experimentInfo.experimentType.steps}
-                </td>
-              </tr>
-
-              <tr>
-                <td className="px-4 py-2 font-semibold">
-                  Onde ele pode ser realizado?
-                </td>
-                <td className="px-4 py-2">
-                  <ul className="list-disc ">
-                    {experimentInfo.topicLocation.map((location) => (
-                      <li key={location.id}>{location.title}</li>
-                    ))}
-                  </ul>
-                </td>
-              </tr>
-             
-              <tr>
-                <td className="px-4 py-2 font-semibold">
-                  Sobre o que é este experimento?
-                </td>
-                <td className="px-4 py-2">
-                  <ul>
-                    {Object.keys(experimentInfo.topicSpecific).map(
-                      (topicKey, index) => (
-                        <li
-                          key={index}
-                          className="bg-gray-200 p-4 mt-4 mb-4 mr-8 flex justify-around"
-                        >
-                          <div className="flex justify-center items-center flex-initial w-1/3">
-                            <span>
-                              <strong>{topicKey}</strong>
-                            </span>
-                          </div>
-                          <div className="flex-initial w-2/3">
-                            <ul className="list-disc">
-                            {experimentInfo.topicSpecific[topicKey as keyof typeof experimentInfo.topicSpecific]?.map(
-  (topic: any) => (
-    <li key={topic.id} className="ml-4">
-      {topic.title}
-    </li>
-  ),
-)}
-
-                            </ul>
-                          </div>
-                        </li>
-                      ),
-                    )}
-                  </ul>
-                </td>
-              </tr>
-
-              <tr>
-                <td className="px-4 py-2 font-semibold">Dificuldade</td>
-                <td className="px-4 py-2">
-                  Este experimento é de dificuldade{" "}
-                  <strong>{experimentInfo.difficulty.title}</strong>, ou seja,{" "}
-                  {experimentInfo.difficulty.explanation}
-                </td>
-              </tr>
-
-
-              <tr>
-                <td className="px-4 py-2 font-semibold">Objetivos</td>
-                <td className="px-4 py-2">
-                  <ul className="list-disc ">
-                    {experimentInfo.objectives.map((objective) => (
-                      <li key={objective.id}>{objective.content}</li>
-                    ))}
-                  </ul>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-
-      <div className="w-full mt-8 flex justify-center items-center flex-col">
-        <div className="w-full mt-8 flex flex-col  border border-gray-300 rounded p-4">
-          <h2 className="text-sm font-semibold mb-6">
-            Lista de materiais utilizados
-          </h2>
-
-          <ul className="list-disc">
-            {experimentInfo.materials.map((material) => (
-              <li key={material.id} className="flex items-center">
-                <FaCheckCircle className="mr-2 text-green-500" />
-                {material.content}
-              </li>
-            ))}
-          </ul>
-        </div>
-
-   
-        <div className="mt-8 w-full border border-gray-300 rounded p-4">
-  <h2 className="text-lg font-semibold mb-6">
-    Métodologia / Passo a passo
-  </h2>
-
-  {experimentInfo.methods.map((method, index) => (
-    <div key={method.id} className="mb-8">
-      <h3 className="text-lg font-semibold mb-2">
-        {index + 1}° Passo
-      </h3>
-
-      <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-start">
-        <div className="w-full sm:w-1/2 sm:mr-4 mb-4 sm:mb-0">
-          <div className="max-w-full">
-            {method.imagePath && (
-              <img
-                src={method.imagePath}
-                alt=""
-                className="w-full h-auto rounded-lg"
-              />
-            )}
-          </div>
-        </div>
-
-        <div className="w-full text-justify">
-          <p className="text-sm lg:text-base">{method.content}</p>
-        </div>
-      </div>
+  <div className="flex justify-between items-initial">
+    <div>
+      <p className="text-gray-600">
+        Publicado em: {experimentInfo.postDate}
+      </p>
+      <p className="text-gray-600">
+        Enviado por: {experimentInfo.profileName}
+      </p>
     </div>
-  ))}
+    <p className="text-blue-500 font-semibold">
+      ID: {experimentInfo.id}
+    </p>
+  </div>
+
+ {/* Título do experimento */}
+<h1 className="text-2xl font-bold mt-6 text-gray-800">{experimentInfo.title}</h1>
+
+{/* Descrição do experimento */}
+<p className="mt-2 text-base text-gray-700">{experimentInfo.description}</p>
+
+{/* Seções Padronizadas */}
+{/* Sobre o que é este experimento? */}
+  <h2 className="text-2xl font-bold mt-6 text-gray-800 mb-4">
+    Sobre o que é este experimento?
+  </h2>
+  <ul className="list-disc pl-4 bg-gray-100 p-4">
+    {Object.keys(experimentInfo.topicSpecific).map((topicKey, index) => (
+      <li key={index} className="mt-2">
+        <strong>{topicKey}:</strong>
+        <ul className="list-disc pl-4 mt-1">
+          {experimentInfo.topicSpecific[topicKey as keyof typeof experimentInfo.topicSpecific]?.map((topic: any) => (
+            <li key={topic.id} className="ml-4">
+              {topic.title}
+            </li>
+          ))}
+        </ul>
+      </li>
+    ))}
+  </ul>
+
+{/* Objetivos */}
+  <h2 className="text-2xl font-bold mt-6 text-gray-800 mb-4">Quais são os objetivos?</h2>
+  <ul className="list-disc pl-4 bg-gray-100 p-4">
+    {experimentInfo.objectives.map((objective) => (
+      <li key={objective.id} className="mt-2">
+        {objective.content}
+      </li>
+    ))}
+  </ul>
+
+
+  
 </div>
 
 
+        
+
+    
 
 
-      </div>
+<div className="w-full mt-8 flex justify-center items-center flex-col">
+<div className="w-full flex flex-col border border-gray-300 rounded p-4 bg-gray-50">
+  <h2 className="text-lg font-semibold mb-6 text-gray-700">
+    <FaClipboardCheck className="inline mr-2" /> {/* Ícone para Lista de Materiais */}
+    Lista de materiais nescessários
+  </h2>
 
-      <div className="mt-4  border border-gray-300 rounded p-4 ">
-        <h2 className="text-sm font-semibold mb-4">
-          Quais os resultados esperados?
-        </h2>
-        <p className="mt-2">{experimentInfo.results}</p>
-      </div>
-      <div className="mt-4  border border-gray-300 rounded p-4">
-        <h2 className="text-sm font-semibold mb-4">
-          Qual a explicação científica?
-        </h2>
-        <p className="mt-2">{experimentInfo.scientificExplanation}</p>
-      </div>
+  <ul className="list-disc pl-4">
+    {experimentInfo.materials.map((material) => (
+      <li key={material.id} className="flex items-center text-lg">
+        <FaCheckCircle className="mr-2 text-green-500" />
+        {material.content}
+      </li>
+    ))}
+  </ul>
+</div>
 
-      <div className="mt-4 border border-gray-300 rounded p-4">
-            <h2 className="text-sm font-semibold mb-4">Documentos para download</h2>
-            <p className="mt-2 cursor-pointer text-blue-600" onClick={() => downloadFile(experimentInfo.activitySheet, 'roteiro-de-atividade.docx')}>
-              roteiro-de-atividade.docx
+
+  <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-start w-full">
+  <div className="mt-8 w-full border border-gray-300 rounded p-4 bg-gray-50">
+    <h2 className="text-lg font-semibold mb-6 text-gray-700">
+      <FaClipboardList className="inline mr-2" />
+      Metodologia
+    </h2>
+
+    {experimentInfo.methods.map((method, index) => (
+      <div key={method.id} className="flex flex-col mb-8 p-4 border border-gray-200 rounded-lg bg-white flex items-start">
+        
+        {/* Bloco para o título da etapa */}
+        <div className="flex-shrink-0 mb-4">
+          <h3 className="text-lg font-semibold text-gray-800">
+            {index + 1}° Etapa
+          </h3>
+        </div>
+
+        {/* Bloco para a imagem e a descrição */}
+        <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-start w-full">
+          <div className="w-full sm:w-1/2 sm:mr-4 mb-4 sm:mb-0">
+            <div className="max-w-full cursor-pointer" onClick={() => openModal(method.imagePath)}>
+              {method.imagePath && (
+                <img
+                  src={method.imagePath}
+                  alt={`Imagem do passo ${index + 1}`}
+                  className="w-full h-auto max-h-80 object-contain rounded-lg shadow"
+                />
+              )}
+              <p className="text-center text-sm text-gray-500 mt-2">(Clique para ampliar)</p>
+            </div>
+          </div>
+
+          <div className="w-full text-justify">
+            <p className="text-lg text-gray-600">
+              <FaArrowRight className="inline mr-2" />
+              {method.content}
             </p>
           </div>
-          
-          <div className="mt-4 border border-gray-300 rounded p-4">
-  <h2 className="text-sm font-semibold mb-4">Referências</h2>
+        </div>
+      </div>
+    ))}
+
+{/* Modal para imagem em fullscreen */}
+{isModalOpen && (
+  <div
+    className="fixed inset-0 bg-black bg-opacity-60 flex justify-center items-center z-50" // Ajuste a opacidade aqui
+    onClick={closeModal} // Mantém a funcionalidade de fechar ao clicar no fundo
+  >
+    <div className="relative" onClick={(e) => e.stopPropagation()}> {/* Impede que o clique no conteúdo do modal feche o modal */}
+      <img
+        src={currentImage}
+        alt="Imagem ampliada"
+        className="max-w-full max-h-screen object-contain"
+      />
+      <button
+        className="absolute top-4 right-4 text-white text-2xl"
+        onClick={closeModal}
+      >
+        <FaTimes />
+      </button>
+    </div>
+  </div>
+)}
+
+  </div>
+</div>
+
+
+</div>
+
+
+{/* Resultados Esperados */}
+<div className="mt-4 border border-gray-300 rounded p-4">
+  <h2 className="text-lg font-semibold mb-4 text-gray-700">
+    <FaChartBar className="inline mr-2" /> {/* Ícone para Resultados Esperados */}
+    Quais os resultados esperados?
+  </h2>
+  <p className="mt-2">{experimentInfo.results}</p>
+</div>
+
+{/* Explicação Científica */}
+<div className="mt-4 border border-gray-300 rounded p-4">
+  <h2 className="text-lg font-semibold mb-4 text-gray-700">
+    <FaFlask className="inline mr-2" /> {/* Ícone para Explicação Científica */}
+    Qual a explicação científica?
+  </h2>
+  <p className="mt-2">{experimentInfo.scientificExplanation}</p>
+</div>
+
+{/* Documentos para Download */}
+<div className="mt-4 border border-gray-300 rounded p-4">
+  <h2 className="text-lg font-semibold mb-4 text-gray-700">
+    <FaFileDownload className="inline mr-2" /> {/* Ícone para Documentos */}
+    Documentos para download
+  </h2>
+  <p
+    className="mt-2 cursor-pointer text-blue-600"
+    onClick={() => downloadFile(experimentInfo.activitySheet, 'roteiro-de-atividade.docx')}
+  >
+    roteiro-de-atividade.docx
+  </p>
+</div>
+
+{/* Referências */}
+<div className="mt-4 border border-gray-300 rounded p-4">
+  <h2 className="text-lg font-semibold mb-4 text-gray-700">
+    <FaBook className="inline mr-2" /> {/* Ícone para Referências */}
+    Referências
+  </h2>
   <ul className="list-disc pl-4 mt-2">
     {experimentInfo.references.map((reference) => (
       <li key={reference.id} className="break-words">
@@ -249,6 +249,7 @@ export default function Experiment({ params }: { params: { slug: string } }) {
     ))}
   </ul>
 </div>
+
 
 
       {/* Adicione mais seções conforme necessário para os outros dados */}
